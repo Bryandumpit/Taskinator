0818c55ac8459724cc007b24c0c300c471ffe9be
Bryan Dumpit <dumpitbr@gmail.com>
2022-07-19 18:41:49 -0400

    add form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
-var buttonE1 = document.querySelector("#save-task"); //points to element with id save-task. This element is a button in index.html
+//var buttonE1 = document.querySelector("#save-task"); //points to element with id save-task. This element is a button in index.html
+var formE1 = document.querySelector("#task-form")
 
-var tasksToDoE1 = document.querySelector("#task-to-do"); //points to element id taskts-to-do. This element is ul in index.html
+var tasksToDoE1 = document.querySelector("#tasks-to-do"); //points to element id taskts-to-do. This element is ul in index.html
 
-var createTaskHandler = function () {   
+var createTaskHandler = function (event) {
+        event.preventDefault();
+        
+
         var listItemE1 = document.createElement("li");
         listItemE1.className = "task-item"; //assigns a class (can be cross-referenced with a css file linked to the same HTML)
         listItemE1.textContent = "This is a new task."; //assigns content (text)
@@ -10,4 +14,4 @@ var createTaskHandler = function () {
 };
 
 
-buttonE1.addEventListener("click", createTaskHandler);
+formE1.addEventListener("submit",createTaskHandler);
