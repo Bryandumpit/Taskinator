71ae5e243cbf33c89268293511cc9bc744465e96
Bryan Dumpit <dumpitbr@gmail.com>
2022-07-20 03:01:06 -0400

    add ability to submit custom tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var formE1 = document.querySelector("#task-form")
 
 var tasksToDoE1 = document.querySelector("#tasks-to-do"); //points to element id taskts-to-do. This element is ul in index.html
 
-var createTaskHandler = function (event) {
+var taskFormHandler = function (event) {
         event.preventDefault();
 
         var taskNameInput = document.querySelector("input[name='task-name']").value;
@@ -12,6 +12,30 @@ var createTaskHandler = function (event) {
         var taskTypeInput = document.querySelector("select[name='task-type']").value;
         console.log(taskTypeInput)
 
+        //check if input values are empty strings
+        if (!taskNameInput || !taskTypeInput) {
+                alert("You need to fill our the task form!")
+                return false;
+        }
+
+        formE1.reset ();
+        
+        //package up data as an object
+        var taskDataObj = {
+                name: taskNameInput,
+                type: taskTypeInput,
+        };
+
+        //send it as an argument to createTaskE1
+
+        createTaskE1(taskDataObj);
+
+        
+};
+
+//holds code that creates new task HTML element
+var createTaskE1 = function(taskDataObj) {
+
         //create list item
         
         var listItemE1 = document.createElement("li");
@@ -19,10 +43,12 @@ var createTaskHandler = function (event) {
 
         //create div to hold task info and add to list item
         var taskInfoE1 = document.createElement("div");
+        
         //give it a class name
         taskInfoE1.className = "task-info";
+        
         //add HTML content to div
-        taskInfoE1.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+        taskInfoE1.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
         
         listItemE1.appendChild(taskInfoE1);
 
@@ -30,7 +56,7 @@ var createTaskHandler = function (event) {
 
         //listItemE1.textContent = taskNameInput; //assigns content (text); this code was commented out because task name input is appended
         tasksToDoE1.appendChild(listItemE1); //appends a child element to tasksTodoE1. the child element is listItemE1
-};
 
+}
 
-formE1.addEventListener("submit",createTaskHandler);
+formE1.addEventListener("submit",taskFormHandler);
